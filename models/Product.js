c2e924fc4cc0b6f69120f92ef0a36c67fd85c5ba
Steve Dever <stephendever.cr@gmail.com>
2021-05-30 13:27:58 -0400

    add fixed datatype to products model

--- models/Product.js
+++ models/Product.js
@@ -24,7 +24,7 @@ Product.init(
     },
 
     price: {
-      type: DataTypes.DECIMAL(10.2),
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true
